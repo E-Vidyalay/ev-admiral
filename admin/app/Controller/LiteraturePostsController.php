eb0576c76c8de27805a47be8818d80ba08ebf587
Jiaana92 <jiaa022@gmail.com>
2015-06-18 12:24:15 +0530

    working on controller

---
+++ admin/app/Controller/LiteraturePostsController.php
@@ -0,0 +1,55 @@
+<?php
+
+class LiteraturesController extends AppController{
+    public $uses=array('User','LiteraturePost','Literature');
+    public function index()
+    {
+        $this->layout='ev_admin';
+        $this->set('literaturePosts',$this->LiteraturePost->find('all'));
+    }
+   public function insert()
+    {
+        $this->layout='ev_admin';
+        $userId = $this->Auth->user('id');
+        $this->set('user', $userId);
+        if($this->request->is('post'))
+        {
+            $data = $this->data;
+           
+            if($this->LiteraturePost->save($data))
+            {
+                $this->Session->setFlash('Literature articel added successfully','default',array('class'=>'alert alert-success'),'success');
+		        $this->redirect(array('controller'=>'LiteraturePosts','action'=>'index'));
+            }
+        }
+    }
+    
+    
+   /* function delete($id=NULL)
+    {
+        $this->Literature->delete($id);
+        $this->Session->setFlash('Literature has been deleted successfully','default',array('class'=>'alert alert-success'),'success');
+        $this->redirect(array('action' => 'index'));
+    }
+    function update($id=NULL){
+        $this->layout='ev_admin';
+        $userId = $this->Auth->user('id');
+        $this->set('user',$userId);
+        if(empty($this->data))
+        {
+            $this->data=$this->Literature->findById($id);
+        }
+        else
+        {
+            $data=$this->data;
+           
+            if($this->Literature->save($data))
+            {
+                $this->Session->setFlash('Literature has been successfully edited','default',array('class'=>'alert alert-success'),'success');
+                $this->redirect(array('action'=>'index'));
+            }
+        }
+    }*/
+    
+}
+
